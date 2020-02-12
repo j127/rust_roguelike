0a7b1b2c5d2a5087d580a08f50604a6df3efb901
J. Cohen <hostelmarketing@gmail.com>
2020-02-11 16:47:59 -0800

    Do hello world

--- hellorust/src/main.rs
+++ hellorust/src/main.rs
@@ -1,3 +1,19 @@
+use rltk::{Rltk, GameState, Console};
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Saluton Mondo!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build();
+    let gs = State {};
+    rltk::main_loop(context, gs);
 }
